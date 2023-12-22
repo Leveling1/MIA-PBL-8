3caea1cdbeb8d5c5ccc81310f750aac931a604a9
Levelin1 <louiskerry.k.guillaume@gmail.com>
2023-12-22 23:41:01 +0100

    Ajout des éléments fais par gloire

---
+++ gloire/Js/app.js
@@ -0,0 +1,16 @@
+// JavaScript
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var revealTop = reveals[i].getBoundingClientRect().top;
+    var revealPoint = 150;
+
+    if (revealTop < windowHeight - revealPoint) {
+      reveals[i].classList.add('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal); 
