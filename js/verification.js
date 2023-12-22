c718983fde3ceab55555669e4a4a04fc7efce1ed
Levelin1 <louiskerry.k.guillaume@gmail.com>
2023-12-22 23:47:33 +0100

    Ajustement

--- js/verification.js
+++ js/verification.js
@@ -8,13 +8,13 @@ form.onsubmit = (e) => {
 }
 continueBtn.onclick = () => {
     let xhr = new XMLHttpRequest();
-    xhr.open("POST", "/php/verification.php", true);
+    xhr.open("POST", "../php/verification.php", true);
     xhr.onload = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 let data = xhr.response;
                 if (data == "success") {
-                    location.href = "/index.html";
+                    location.href = "../home.html";
                 } else {
                     errorText.textContent = data;
                     errorText.style.display = "block";
